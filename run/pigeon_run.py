36a3b3e1fc03eeee1c329fede852ccdaff648887
johnlime <thelimejuiceproductions@gmail.com>
2021-11-08 13:07:25 +0900

    added reward code as an argument for running pigeon_run.py

--- run/pigeon_run.py
+++ run/pigeon_run.py
@@ -5,8 +5,8 @@ import numpy as np
 import sys
 sys.path.append('src/rlkit_ppo')
 
-def run_rand_policy(body_speed = 0.0):
-    env = PigeonEnv3Joints(body_speed)
+def run_rand_policy(body_speed, reward_code):
+    env = PigeonEnv3Joints(body_speed, reward_code)
     observation = env.reset()
     for t in range(1000):
         env.render()
@@ -15,8 +15,8 @@ def run_rand_policy(body_speed = 0.0):
         print(reward)
     env.close()
 
-def run_trained_policy(policy, body_speed):
-    env = PigeonEnv3Joints(body_speed)
+def run_trained_policy(policy, body_speed, reward_code):
+    env = PigeonEnv3Joints(body_speed, reward_code)
     observation = env.reset()
     for t in range(1000):
         env.render()
@@ -32,12 +32,15 @@ if __name__ == "__main__":
                         help='path to the snapshot file')
     parser.add_argument('--body_speed', type=float, default=0.0,
                         help='pigeon body speed')
+    parser.add_argument('--reward_code', type=str,
+                        default="head_stable_manual_reposition",
+                        help='specify reward function')
     args = parser.parse_args()
 
     if args.policy_file is None:
-        run_rand_policy(args.body_speed)
+        run_rand_policy(args.body_speed, args.reward_code)
 
     else:
         policy = torch.load(args.policy_file,
                             map_location=torch.device('cpu'))
-        run_trained_policy(policy, args.body_speed)
+        run_trained_policy(policy, args.body_speed, args.reward_code)

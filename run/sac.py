e363340ee3040d081e18eb9716d4ee9d46aa28b9
johnlime <thelimejuiceproductions@gmail.com>
2021-11-28 13:55:07 +0900

    added SAC for pigeon

--- run/sac.py
+++ run/sac.py
@@ -1,33 +1,41 @@
 from gym_env.pigeon_gym import PigeonEnv3Joints
-#import gym
-
-import sys
-sys.path.append('src/rlkit_ppo')
 
 import rlkit.torch.pytorch_util as ptu
-from rlkit.torch.ppo.ppo_env_replay_buffer import PPOEnvReplayBuffer
+from rlkit.data_management.env_replay_buffer import EnvReplayBuffer
 from rlkit.envs.wrappers import NormalizedBoxEnv
 from rlkit.launchers.launcher_util import setup_logger
-from rlkit.torch.ppo.ppo_path_collector import PPOMdpPathCollector
-from rlkit.torch.ppo.policies import TanhGaussianPolicy, MakeDeterministic
-from rlkit.torch.ppo.ppo import PPOTrainer
+from rlkit.samplers.data_collector import MdpPathCollector
+from rlkit.torch.sac.policies import TanhGaussianPolicy, MakeDeterministic
+from rlkit.torch.sac.sac import SACTrainer
 from rlkit.torch.networks import FlattenMlp
-from rlkit.torch.ppo.ppo_torch_batch_rl_algorithm import PPOTorchBatchRLAlgorithm
+from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
 
-import torch
 import argparse
 
 def experiment(variant, args):
-    torch.autograd.set_detect_anomaly(True)
-    # pigeon moves at speed of 10
     expl_env = NormalizedBoxEnv(PigeonEnv3Joints(args.body_speed, args.reward_code, args.max_offset))
     eval_env = NormalizedBoxEnv(PigeonEnv3Joints(args.body_speed, args.reward_code, args.max_offset))
     obs_dim = expl_env.observation_space.low.size
     action_dim = eval_env.action_space.low.size
 
     M = variant['layer_size']
-    vf = FlattenMlp(
-        input_size=obs_dim,
+    qf1 = FlattenMlp(
+        input_size=obs_dim + action_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    qf2 = FlattenMlp(
+        input_size=obs_dim + action_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    target_qf1 = FlattenMlp(
+        input_size=obs_dim + action_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    target_qf2 = FlattenMlp(
+        input_size=obs_dim + action_dim,
         output_size=1,
         hidden_sizes=[M, M],
     )
@@ -37,35 +45,33 @@ def experiment(variant, args):
         hidden_sizes=[M, M],
     )
     eval_policy = MakeDeterministic(policy)
-    eval_step_collector = PPOMdpPathCollector(
+    eval_path_collector = MdpPathCollector(
         eval_env,
         eval_policy,
-        calculate_advantages=False
     )
-    expl_step_collector = PPOMdpPathCollector(
+    expl_path_collector = MdpPathCollector(
         expl_env,
         policy,
-        calculate_advantages=True,
-        vf=vf,
-        gae_lambda=0.97,
-        discount=0.995,
     )
-    replay_buffer = PPOEnvReplayBuffer(
+    replay_buffer = EnvReplayBuffer(
         variant['replay_buffer_size'],
         expl_env,
     )
-    trainer = PPOTrainer(
+    trainer = SACTrainer(
         env=eval_env,
         policy=policy,
-        vf=vf,
+        qf1=qf1,
+        qf2=qf2,
+        target_qf1=target_qf1,
+        target_qf2=target_qf2,
         **variant['trainer_kwargs']
     )
-    algorithm = PPOTorchBatchRLAlgorithm(
+    algorithm = TorchBatchRLAlgorithm(
         trainer=trainer,
         exploration_env=expl_env,
         evaluation_env=eval_env,
-        exploration_data_collector=expl_step_collector,
-        evaluation_data_collector=eval_step_collector,
+        exploration_data_collector=expl_path_collector,
+        evaluation_data_collector=eval_path_collector,
         replay_buffer=replay_buffer,
         **variant['algorithm_kwargs']
     )
@@ -88,34 +94,33 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # noinspection PyTypeChecker
-    T = 2048
-    max_ep_len = 1000
-    epochs = 10
-    minibatch_size = 64
-
     variant = dict(
-        algorithm="PPO",
+        algorithm="SAC",
         version="normal",
-        layer_size=64,
-        replay_buffer_size=T,
+        layer_size=256,
+        replay_buffer_size=int(1E6),
         algorithm_kwargs=dict(
-            num_iter=int(2 * 1e6 // T),
-            num_eval_steps_per_epoch=max_ep_len,
-            num_trains_per_train_loop=T // minibatch_size * epochs,
-            num_expl_steps_per_train_loop=T,
-            min_num_steps_before_training=0,
-            max_path_length=max_ep_len,
-            minibatch_size=minibatch_size,
+            num_epochs=3000,
+            num_eval_steps_per_epoch=5000,
+            num_trains_per_train_loop=1000,
+            num_expl_steps_per_train_loop=1000,
+            min_num_steps_before_training=1000,
+            max_path_length=1000,
+            batch_size=256,
         ),
         trainer_kwargs=dict(
-            epsilon=0.2,
-            reward_scale=1.0,
-            lr=3e-4,
+            discount=0.99,
+            soft_target_tau=5e-3,
+            target_update_period=1,
+            policy_lr=3E-4,
+            qf_lr=3E-4,
+            reward_scale=1,
+            use_automatic_entropy_tuning=True,
         ),
     )
 
     # setting up argparse params (body speed and reward function)
-    setup_logger('ppo_pigeon_3_joints_' + args.reward_code + \
+    setup_logger('sac_pigeon_3_joints_' + args.reward_code + \
                  '_body_speed_' + str(args.body_speed) + \
                  '_max_offset_' + str(args.max_offset),
                  variant=variant)

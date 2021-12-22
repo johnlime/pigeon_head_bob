f33d167a64d1ffd3b79172a492dc8a05a9e87cd0
johnlime <thelimejuiceproductions@gmail.com>
2021-12-22 19:30:33 +0900

    num_epochs=3000 (Please review specific meanings of params in SAC)

--- run/sac.py
+++ run/sac.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
         layer_size=256,
         replay_buffer_size=int(1E6),
         algorithm_kwargs=dict(
-            num_epochs=2000, #3000,
+            num_epochs=3000,
             num_eval_steps_per_epoch=5000,
             num_trains_per_train_loop=1000,
             num_expl_steps_per_train_loop=1000,
